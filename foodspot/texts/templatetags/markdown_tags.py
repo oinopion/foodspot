ac47d8976cfdb272df3e2e2e52fa892b15eb83fb
Tomek Paczkowski <tomek@hauru.eu>
2014-02-02 22:16:50 +0100

    Added Markdown extension for instagram short tags

--- foodspot/texts/templatetags/markdown_tags.py
+++ foodspot/texts/templatetags/markdown_tags.py
@@ -1,9 +1,29 @@
 import markdown
 from django import template
+from markdown.util import etree
+from markdown.inlinepatterns import Pattern
+from markdown.extensions import Extension
 
 register = template.Library()
 
 
 @register.simple_tag(name='markdown')
 def render_markdown(text):
-    return markdown.markdown(text)
+    return markdown.markdown(text, extensions=[InstagramExtension()])
+
+
+class InstagramPattern(Pattern):
+    REDIRECT_URL = '//instagram.com/p/%s/media/?size=l'
+    PATTERN = r'\[instagram:(?P<id>[^\]]+)\]'
+
+    def __init__(self, markdown_instance=None):
+        super().__init__(self.PATTERN, markdown_instance)
+
+    def handleMatch(self, m):
+        href = self.REDIRECT_URL % m.group('id')
+        return etree.Element('img', attrib={'src': href, 'class': 'instagram'})
+
+
+class InstagramExtension(Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.inlinePatterns.add('instagram', InstagramPattern(md), '_begin')
--- foodspot/texts/tests/test_templatetags.py
+++ foodspot/texts/tests/test_templatetags.py
@@ -1,5 +1,7 @@
+import re
 from django import template
 from django.test import TestCase
+from ..templatetags.markdown_tags import InstagramPattern
 
 
 class MarkdownTest(TestCase):
@@ -7,7 +9,33 @@ class MarkdownTest(TestCase):
         html = self.render("{% markdown article.text %}", {})
         self.assertEqual("", html)
 
+    def test_has_instagram_extension(self):
+        text = "[instagram:hello]"
+        html = self.render("{% markdown text %}", {'text': text})
+        self.assertIn('instagram.com/p/hello/media', html)
+
     def render(self, template_text, context):
         t = template.Template("{% load markdown_tags %}" + template_text)
         c = template.Context(context)
         return t.render(c)
+
+
+class InstagramPatternTest(TestCase):
+    regexp = re.compile(InstagramPattern.PATTERN)
+
+    def test_regexp(self):
+        self.assertRegex('[instagram:abc]', self.regexp)
+        self.assertRegex('[instagram:yiuh1y]', self.regexp)
+        self.assertNotRegex('[instagram:]', self.regexp)
+        self.assertNotRegex('![link](http://example.com)', self.regexp)
+
+    def test_handle_match(self):
+        pattern = InstagramPattern()
+        match = self.regexp.match('[instagram:hello]')
+        element = pattern.handleMatch(match)
+        self.assertEqual('img', element.tag)
+        self.assertEqual('instagram', element.attrib.get('class'))
+        self.assertEqual(
+            '//instagram.com/p/hello/media/?size=l',
+            element.get('src')
+        )
