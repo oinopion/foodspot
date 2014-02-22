97969272c8ddf8d153133a95e5f254d17be6af0f
Tomek Paczkowski <tomek@hauru.eu>
2014-02-22 19:52:04 +0100

    Changed blog title

--- foodspot/views.py
+++ foodspot/views.py
@@ -3,7 +3,7 @@ from foodspot.texts.models import Article
 from foodspot.texts.views import render_articles
 
 
-@cache_control(max_age=60 * 5, public=True)
+@cache_control(max_age=60, public=True)
 def home_page(request):
     latest_articles = Article.published.all()[:5]
     return render_articles(latest_articles, 'home.html', {
