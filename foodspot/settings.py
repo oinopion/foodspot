b506bd0874e39ab83bc6aacdb7ce9da5289f182b
Tomek Paczkowski <tomek@hauru.eu>
2014-02-23 17:59:06 +0100

    Removed djorm_pool - breaks tests

--- foodspot/settings.py
+++ foodspot/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.redirects',
     'raven.contrib.django.raven_compat',
-    'djorm_pool',
     'debug_toolbar',
     'foodspot.texts',
 )
