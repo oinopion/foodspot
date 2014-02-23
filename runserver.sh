99514ec7cf74f3b647b93e5fb5a5aa25279f07da
Tomek Paczkowski <tomek@hauru.eu>
2014-02-23 15:47:00 +0100

    Added newrelic

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-exec venv/bin/gunicorn --config conf/gun.py foodspot.wsgi:application
+NEW_RELIC_CONFIG_FILE=conf/newrelic.ini
+GUNICORN="venv/bin/gunicorn --config conf/gun.py"
+DJANGO_APP=foodspot.wsgi:application
+
+export NEW_RELIC_CONFIG_FILE
+
+exec newrelic-admin run-program $GUNICORN $DJANGO_APP
