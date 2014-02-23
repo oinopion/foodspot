a09e96fb30c6dfa8b7d24e7b8d388c581f2a64f6
Tomek Paczkowski <tomek@hauru.eu>
2014-02-23 15:49:42 +0100

    Fixed runserver script

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 NEW_RELIC_CONFIG_FILE=conf/newrelic.ini
+NEW_RELIC_ADMIN="venv/bin/newrelic-admin"
 GUNICORN="venv/bin/gunicorn --config conf/gun.py"
 DJANGO_APP=foodspot.wsgi:application
 
 export NEW_RELIC_CONFIG_FILE
 
-exec newrelic-admin run-program $GUNICORN $DJANGO_APP
+exec $NEW_RELIC_ADMIN run-program $GUNICORN $DJANGO_APP
